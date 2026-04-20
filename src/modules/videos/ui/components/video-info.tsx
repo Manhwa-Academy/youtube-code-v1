2652f34e81072e0fc79f1c571058d003d30802ef
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 20:42:34 +0700

    Update video-info.tsx

--- src/modules/videos/ui/components/video-info.tsx
+++ src/modules/videos/ui/components/video-info.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import { useMemo } from "react";
 import { formatDistanceToNow } from "date-fns";
+import { vi } from "date-fns/locale";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { UserAvatar } from "@/components/user-avatar";
-
 import { UserInfo } from "@/modules/users/ui/components/user-info";
-
 import { VideoMenu } from "./video-menu";
 import { VideoGetManyOutput } from "../../types";
 
@@ -15,27 +14,18 @@ interface VideoInfoProps {
   onRemove?: () => void;
 };
 
-export const VideoInfoSkeleton = () => {
-  return (
-    <div className="flex gap-3">
-      <Skeleton className="size-10 flex-shrink-0 rounded-full" />
-      <div className="min-w-0 flex-1 space-y-2">
-        <Skeleton className="h-5 w-[90%]" />
-        <Skeleton className="h-5 w-[70%]" />
-      </div>
-    </div>
-  );
-};
-
 export const VideoInfo = ({ data, onRemove }: VideoInfoProps) => {
   const compactViews = useMemo(() => {
-    return Intl.NumberFormat("en", {
-      notation: "compact"
+    return new Intl.NumberFormat("vi-VN", {
+      notation: "compact",
     }).format(data.viewCount);
   }, [data.viewCount]);
 
   const compactDate = useMemo(() => {
-    return formatDistanceToNow(data.createdAt, { addSuffix: true });
+    return formatDistanceToNow(data.createdAt, {
+      addSuffix: true,
+      locale: vi,
+    });
   }, [data.createdAt]);
 
   return (
@@ -43,24 +33,28 @@ export const VideoInfo = ({ data, onRemove }: VideoInfoProps) => {
       <Link prefetch href={`/users/${data.user.id}`}>
         <UserAvatar imageUrl={data.user.imageUrl} name={data.user.name} />
       </Link>
+
       <div className="min-w-0 flex-1">
         <Link prefetch href={`/videos/${data.id}`}>
           <h3 className="font-medium line-clamp-1 lg:line-clamp-2 text-base break-words">
             {data.title}
           </h3>
         </Link>
+
         <Link prefetch href={`/users/${data.user.id}`}>
           <UserInfo name={data.user.name} />
         </Link>
+
         <Link prefetch href={`/videos/${data.id}`}>
           <p className="text-sm text-gray-600 line-clamp-1">
-            {compactViews} views • {compactDate}
+            {compactViews} lượt xem • {compactDate}
           </p>
         </Link>
       </div>
+
       <div className="flex-shrink-0">
         <VideoMenu videoId={data.id} onRemove={onRemove} />
       </div>
     </div>
-  )
+  );
 };
