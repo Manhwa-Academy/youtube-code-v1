570ff612900de64c8f43015f3673b9f084b4d835
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 20:30:26 +0700

    Update studio-view.tsx

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -4,9 +4,9 @@ export const StudioView = () => {
   return ( 
     <div className="flex flex-col gap-y-6 pt-2.5">
       <div className="px-4">
-        <h1 className="text-2xl font-bold">Channel content</h1>
+        <h1 className="text-2xl font-bold">Nội dung kênh</h1>
         <p className="text-xs text-muted-foreground">
-          Manage your channel content and videos
+          Quản lý video và nội dung trên kênh của bạn
         </p>
       </div>
       <VideosSection />
