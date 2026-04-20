4ee32ac1835a8295a1f7e6c91efad1de7c32dce3
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 19:29:38 +0700

    Update subscribed-view.tsx

--- src/modules/home/ui/views/subscribed-view.tsx
+++ src/modules/home/ui/views/subscribed-view.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import { SubscribedVideosSection } from "../sections/subscribed-videos-section";
 
 export const SubscribedView = () => {
   return (
     <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
       <div>
-        <h1 className="text-2xl font-bold">Subscribed</h1>
+        <h1 className="text-2xl font-bold">Kênh đăng ký</h1>
         <p className="text-xs text-muted-foreground">
-          Videos from your favorite creators
+          Video từ các kênh bạn đã đăng ký
         </p>
       </div>
       <SubscribedVideosSection />
