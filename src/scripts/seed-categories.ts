9e3670c45b2fe3828df5ca8caa31c6bebc21fec6
Phong <164517212+Manhwa-Academy@users.noreply.github.com>
2026-04-20 18:20:06 +0700

    Update seed-categories.ts

--- src/scripts/seed-categories.ts
+++ src/scripts/seed-categories.ts
@@ -2,20 +2,20 @@ import { db } from "@/db";
 import { categories } from "@/db/schema";
 
 const categoryNames = [
-  "Cars and vehicles",
-  "Comedy",
-  "Education",
-  "Gaming",
-  "Entertainment",
-  "Film and animation",
-  "How-to and style",
-  "Music",
-  "News and politics",
-  "People and blogs",
-  "Pets and animals",
-  "Science and technology",
-  "Sports",
-  "Travel and events",
+  "Ô tô và xe cộ",
+  "Hài kịch",
+  "Giáo dục",
+  "Trò chơi",
+  "Giải trí",
+  "Phim và hoạt hình",
+  "Hướng dẫn và phong cách",
+  "Âm nhạc",
+  "Tin tức và chính trị",
+  "Mọi người và blog",
+  "Thú cưng và động vật",
+  "Khoa học và công nghệ",
+  "Thể thao",
+  "Du lịch và sự kiện",
 ];
 
 async function main() {
@@ -24,12 +24,12 @@ async function main() {
   try {
     const values = categoryNames.map((name) => ({
       name,
-      description: `Videos related to ${name.toLowerCase()}`,
+      description: `Video thuộc danh mục ${name.toLowerCase()}`,
     }));
 
     await db.insert(categories).values(values);
 
-    console.log("Categores seeded successfully!");
+    console.log("Categories seeded successfully!");
   } catch (error) {
     console.error("Error seeding categories: ", error);
     process.exit(1);
